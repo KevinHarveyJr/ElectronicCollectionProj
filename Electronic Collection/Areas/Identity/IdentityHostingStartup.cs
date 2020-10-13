e31ee6fa5a41570f87daa766c55a974da4c956ce
Kevin Harvey Jr <lordty@gmail.com>
2020-10-13 13:02:38 -0500

    Initial_Commit

---
+++ Electronic Collection/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Electronic_Collection.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Electronic_Collection.Areas.Identity.IdentityHostingStartup))]
+namespace Electronic_Collection.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
