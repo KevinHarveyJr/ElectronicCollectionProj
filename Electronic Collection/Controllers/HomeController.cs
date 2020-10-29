06a19e5fb2a6c9a82943e29fe8b4b736114130f1
Kevin Harvey Jr <lordty@gmail.com>
2020-10-29 00:42:01 -0500

    Finished_Adding_SignalR/Added_Messages_View

--- Electronic Collection/Controllers/HomeController.cs
+++ Electronic Collection/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Electronic_Collection.Controllers
             return View();
         }
 
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
